2950635f15ea725814c66692e96de408d46edb7e
meetpatel1284 <your.email@example.com>
2026-06-18 16:16:23 +0530

    Fixed static storage - no caching

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -137,7 +137,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
    "staticfiles": {
-    "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+    "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
 },
 }
 # Default primary key field type
